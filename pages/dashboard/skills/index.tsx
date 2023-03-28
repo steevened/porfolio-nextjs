2896080e1f6b4d241fa334ad90941f380f6aeac1
steven <alvaradosteven315@gmail.com>
2023-03-28 12:25:57 -0500

    configure firebase

--- pages/dashboard/skills/index.tsx
+++ pages/dashboard/skills/index.tsx
@@ -1,57 +1,78 @@
 import { NextPageWithLayout } from '../../_app';
 import DashboardLayout from '../../../components/layouts/DashboardLayout';
-import { FormEvent, ReactNode, useState } from 'react';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { FormEvent, ReactNode, useState, useEffect } from 'react';
+import {
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+} from 'firebase/storage';
 import AddCard from '../../../components/atoms/Cards/AddCard';
 import { Button } from '@mui/material';
 import Input from '@/components/atoms/inputs/Input';
-import { storage } from '@/lib/firebase/config';
+import { db, storage } from '@/lib/firebase/config';
+import Image from 'next/image';
+import { doc, setDoc } from 'firebase/firestore/lite';
 
 const Skills: NextPageWithLayout = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const [skillName, setSkillName] = useState<string>('');
   const [skillImage, setSkillImage] = useState<File | null>(null);
+  const [data, setData] = useState<any>(null);
 
-  const handleAddSkill = (e: FormEvent) => {
+  const [progress, setProgress] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (!skillImage) return;
+    const imgName = new Date().getTime() + skillImage?.name;
+
+    const uploadFile = () => {
+      const storageRef = ref(storage, skillImage.name);
+
+      const uploadFile = uploadBytesResumable(storageRef, skillImage);
+
+      uploadFile.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is' + progress + '% done');
+          setProgress(progress);
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadFile.snapshot.ref).then((downloadUrl) => {
+            setData({ skillName, skillImage: downloadUrl });
+          });
+        }
+      );
+    };
+    skillImage && uploadFile();
+  }, [skillImage]);
+
+  async function handleAddSkill(e: FormEvent) {
     e.preventDefault();
 
-    const storageRef = ref(storage, `skills/${skillImage?.name}`);
-    uploadBytes(storageRef, skillImage!).then(
-      (snapshot) => {
-        getDownloadURL(
-          (snapshot.ref as any).then(
-            (url: any) => {
-              saveSkill({
-                name,
-                image: url,
-              });
-            },
-            (error: any) => {
-              console.log(error);
-              saveSkill({
-                name,
-                image: null,
-              });
-            }
-          )
-        );
-      },
-      (error: any) => {
-        console.log(error);
-        saveSkill({
-          name,
-          image: null,
-        });
-      }
-    );
-  };
-
-  function saveSkill(portfolio: any) {
-    console.log(portfolio);
+    try {
+      const response = await setDoc(doc(db, 'skills'), data);
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-
-  console.log(skillName, skillImage);
 
   return (
     <div className="">
@@ -84,8 +105,8 @@ const Skills: NextPageWithLayout = () => {
                   Add a technologie
                 </h3>
               </div>
-              <div className="mx-5 mt-10 flex flex-col">
-                <label className="block text-start pb-1 text-sm font-semibold ">
+              <div className="flex flex-col mx-5 mt-10">
+                <label className="block pb-1 text-sm font-semibold text-start ">
                   <span>Name</span>
                   <Input
                     onChange={(e) => setSkillName(e.target.value)}
@@ -94,10 +115,20 @@ const Skills: NextPageWithLayout = () => {
                 </label>
                 <label
                   htmlFor="file"
-                  className="w-max text-start space-y-2 text-sm font-semibold"
+                  className="object-cover w-32 h-32 space-y-2 text-sm font-semibold text-start"
                 >
                   <span className="mb-10">Image</span>
-                  <AddCard />
+                  {skillImage ? (
+                    <Image
+                      width={100}
+                      height={100}
+                      src={URL.createObjectURL(skillImage)}
+                      alt="skill"
+                      className="w-full h-full "
+                    />
+                  ) : (
+                    <AddCard />
+                  )}
                   <input
                     accept="image/*"
                     type="file"
