be777db0106546fb60d6a6b224e326c307a87f96
steven <alvaradosteven315@gmail.com>
2023-04-12 11:16:25 -0500

    build sidebar drawer

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,15 +1,24 @@
-import { useRouter } from 'next/router';
 import { ReactNode, useContext } from 'react';
 import Navbar from './Navbar';
-import gsap from 'gsap';
 import { UIContext } from '@/context/ui';
+import Drawer from './Drawer';
+import Overlay from './Overlay';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const { sideMenuOpen } = useContext(UIContext);
   return (
-    <div className="">
+    <div className="overflow-x-hidden ">
       <Navbar />
+      <Drawer />
+      <Overlay />
 
-      <main className="max-w-3xl px-5 mx-auto ">{children}</main>
+      <main
+        className={`max-w-3xl px-5 mx-auto duration-500 ${
+          sideMenuOpen && 'translate-x-5'
+        }`}
+      >
+        {children}
+      </main>
     </div>
   );
 };
--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,51 +1,14 @@
 import { UIContext } from '@/context/ui';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FC, MouseEvent, useContext, useEffect, useRef, useState } from 'react';
-import { gsap } from 'gsap';
-import {
-  MoonIcon,
-  MoonIconOut,
-  SunIcon,
-  SunIconOut,
-  SystemIcon,
-  SystemOut,
-} from '../svg/Svg';
-import { useTheme } from 'next-themes';
-
-interface MenuItem {
-  name: string;
-  href: string;
-}
-
-const menuItems: MenuItem[] = [
-  {
-    name: 'About',
-    href: '/about',
-  },
-  {
-    name: 'Works',
-    href: '/works',
-  },
-  {
-    name: 'Blogs',
-    href: '/blogs',
-  },
-  {
-    name: 'Contact',
-    href: '/contact',
-  },
-];
+import { FC, useContext, useEffect, useState } from 'react';
+import Drawer from './Drawer';
 
 const Navbar: FC = () => {
   const [hoverIndex, setHoverIndex] = useState<number>(-1);
   const [routeIndex, setRouteIndex] = useState<number>(-1);
-  const navbarMobileRef = useRef<HTMLDivElement>(null);
 
   // const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const { theme, setTheme } = useTheme();
-
-  console.log(theme);
 
   const { sideMenuOpen, toggleSideMenu, closeSideMenu } = useContext(UIContext);
 
@@ -89,47 +52,9 @@ const Navbar: FC = () => {
     }
   }, [pathname]);
 
-  // animater the navbar
-
-  function animateNavbar() {
-    // Get a reference to the second navbar element
-    const navbarMobile = navbarMobileRef.current;
-
-    // Define the animation parameters
-    const animationParams = {
-      duration: 0.5,
-      ease: 'power2.inOut',
-      y: sideMenuOpen ? 0 : '-100%',
-      opacity: sideMenuOpen ? 1 : 0,
-    };
-
-    // Animate the navbar
-    gsap.to(navbarMobile, animationParams);
-  }
-
-  useEffect(() => {
-    animateNavbar();
-  }, [sideMenuOpen]);
-
-  useEffect(() => {
-    function handleClickOutside(event: any) {
-      const navbarMobile = navbarMobileRef.current;
-      const menuButton = event.target.closest('.menu-button');
-      if (navbarMobile && !navbarMobile.contains(event.target) && !menuButton) {
-        closeSideMenu();
-      }
-    }
-
-    window.addEventListener('click', handleClickOutside);
-
-    return () => {
-      window.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-
   return (
     <div className="relative">
-      <nav className="z-50 flex items-center gap-5 p-5 bg-white/75 dark:bg-black/75 backdrop-blur-xl dark:backdrop-blur-lg dark:shadow-app-bottom shadow-app-bottom-light sticky">
+      <nav className="sticky z-30 flex items-center gap-5 p-5 bg-white/75 dark:bg-black/75 backdrop-blur-xl dark:backdrop-blur-lg dark:shadow-app-bottom shadow-app-bottom-light">
         <Link href="/" className=" grow">
           <p className="font-semibold">STEVEN ELIAS</p>
         </Link>
@@ -224,43 +149,7 @@ const Navbar: FC = () => {
           />
         </div>
       </nav>
-      <div
-        className="absolute inset-x-0 bg-zinc-100 dark:bg-app-gray navbar-mobile sm:max-w-md sm:ml-auto  z-40 shadow-app-bottom-light dark:shadow-app-bottom"
-        ref={navbarMobileRef}
-      >
-        <div className="flex flex-col gap-3 px-5 py-3 ">
-          {menuItems.map((item, i) => (
-            <Link
-              key={i}
-              href={item.href}
-              className=" py-3 rounded-sm  text-center  w-full hover:bg-zinc-200   dark:hover:bg-black duration-100 shadow-app-shadow-light dark:shadow-app-shadow hover:shadow-app-shadow-hover sm:hidden"
-              role="button"
-            >
-              {item.name}
-            </Link>
-          ))}
-          <div className="rounded-sm  text-center  w-full  duration-100 dark:shadow-app-shadow  flex justify-between shadow-app-shadow-light">
-            <button
-              onClick={() => setTheme('system')}
-              className="flex-1 flex justify-center items-center py-3 hover:shadow-app-shadow-hover dark:hover:bg-black duration-100 hover:bg-zinc-200"
-            >
-              {theme === 'system' ? <SystemOut /> : <SystemIcon />}
-            </button>
-            <button
-              className="flex-1 flex justify-center items-center py-3 hover:shadow-app-shadow-hover dark:hover:bg-black duration-100 hover:bg-zinc-200"
-              onClick={() => setTheme('light')}
-            >
-              {theme === 'light' ? <SunIconOut /> : <SunIcon />}
-            </button>
-            <button
-              className="flex-1 flex justify-center items-center py-3 hover:shadow-app-shadow-hover dark:hover:bg-black duration-100 hover:bg-zinc-200"
-              onClick={() => setTheme('dark')}
-            >
-              {theme === 'dark' ? <MoonIconOut /> : <MoonIcon />}
-            </button>
-          </div>
-        </div>
-      </div>
+      {/* sidebar */}
     </div>
   );
 };
